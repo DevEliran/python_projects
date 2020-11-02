8756d54d146b23a9b18e97ec5f91e9bc874dd6fa
Eliran T <dev.eliran96@gmail.com>
2020-11-02 11:02:43 +0200

    small updates

--- motion_detector/log_n_plot.py
+++ motion_detector/log_n_plot.py
@@ -1,16 +1,19 @@
 from motion_detector import df
-from bokeh.plotting import figure , show , output_file
-from bokeh.models import HoverTool , ColumnDataSource
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
 
-df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
-df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
-cds=ColumnDataSource(df)
+df["Start_formatted"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_formatted"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+cds = ColumnDataSource(df)
 
-p=figure(x_axis_type='datetime',height=500,width=1500,title="Motion detector graph")
-p.yaxis.minor_tick_line_color=None
-p.ygrid[0].ticker.desired_num_ticks=1
-hover=HoverTool(tooltips=[("Start ","@Start_string"),("End ","@End_string")])
-p.add_tools(hover)
-q=p.quad(left="Start",right="End",bottom=0,top=1,color="lightgrey",source=cds)
+fig = figure(x_axis_type='datetime', height=500, width=800,
+             title="Motion detector graph", align='center')
+fig.yaxis.minor_tick_line_color = None
+fig.ygrid[0].ticker.desired_num_ticks = 1
+hover = HoverTool(tooltips=[("Start ", "@Start_formatted"),
+                            ("End ", "@End_formatted")])
+fig.add_tools(hover)
+q = fig.quad(left="Start", right="End", bottom=0, top=1, color="cyan",
+             source=cds)
 output_file("graph.html")
-show (p)
+show(fig)
